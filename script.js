93662894205625554a76a55dbf577d62f6ac370e
Sergio Suarique <ss_1913@outlook.com>
2023-06-15 13:03:56 -0500

    basic calculator v1.0

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-// var u_name = prompt("what's r name?");
-// document.write("Hey "+u_name+"!");
-// var array = ["Sergio","Suarique", 26, true];
-// array.push(1);
-// array.unshift("0");
-// array.pop();
-// array.shift();
-// array.forEach( t => console.log(t+"\n"));
-// const container = document.getElementById("container");
-// container.style.width = "300px";
-// container.style.height = "300px";
-// container.style.background = "#ff0";
-// const entrada = document.getElementById("input");
-// entrada.value = "HOLA";
-
 const buttons = document.getElementsByClassName("button");
-const display = document.getElementById("display");
+const input = document.getElementById("input");
+const output = document.getElementById("output");
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("mousedown", () => {
@@ -29,15 +15,16 @@ for (let i = 0; i < buttons.length; i++) {
 
     buttons[i].addEventListener("click", () => {
         if(buttons[i].classList.contains("digit")){
-            display.textContent += buttons[i].textContent;
+            input.textContent += buttons[i].textContent;
         }else if(buttons[i].textContent === "clr"){
-            display.textContent = "";
+            input.textContent = "";
+            output.textContent = "";
         }else if(buttons[i].textContent === "←"){
-            display.textContent = display.textContent.substring(0,display.textContent.length-1);
+            input.textContent = input.textContent.substring(0,input.textContent.length-1);
         }else if(buttons[i].textContent === "="){
-            opParts = split(display.textContent);
+            opParts = split(input.textContent);
             
-            operate(opParts.digits,opParts.symbols);
+            calc(opParts.digits,opParts.symbols);
         }
     });
   }
@@ -60,46 +47,44 @@ function split(operation){
     return {symbols,digits};
 }
 
-function isMathSymbol(char) {
-    return char === "+" || char === "-" || char === "*" || char === "/";
-}
-
-//5+8*3/2-6*2 = 5
-//0 1 2 3 4 5
-//5 8 3 2 6 2
-//+ * / - *
-function operate(digits, symbols){
-    console.log(digits);
-    console.log("--------------------")
-    console.log(symbols);
+function calc(digits, symbols){
+    let sym = [];
+    let dig = [];
     let digAux = [];
     let symAux = [];
-    for(let i = 0; i < symbols.length; i++){
-        let symbol = symbols[i];
+    let i;
+    let symbol;
+    for(i = 0; i < symbols.length; i++){
+        symbol = symbols[i];
         if(symbol === "*" || symbol === "/"){
-            if(i > 0 && symbols[i-1] !== "*" && symbols[i-1] !== "/")
+            if((i > 0 && symbols[i-1] !== "*" && symbols[i-1] !== "/") || i == 0)
                 digAux.push(digits[i]);
             symAux.push(symbol);
             digAux.push(digits[i+1]);
-
         }else{
             if(digAux.length > 0){
-                console.log(operate2(digAux,symAux))
+                dig.push(operate(digAux,symAux));
                 digAux = [];
                 symAux = [];
+            }else{
+                dig.push(digits[i]);
             }
+            sym.push(symbol);
         }
     }
     if(digAux.length > 0){
-        console.log(operate2(digAux,symAux))
+        dig.push(operate(digAux,symAux));
         digAux = [];
         symAux = [];
+    }else{
+        dig.push(digits[i]);
     }
 
-    
+    output.textContent = operate(dig,sym);
+
 }
-function operate2(dig, sym){
-    let result = dig[0];
+function operate(dig, sym){
+    let result = parseFloat(dig[0]);
     for(let i = 1; i < dig.length; i++){
         const symbol = sym[i-1];
         const digit = parseFloat(dig[i]);
@@ -107,7 +92,15 @@ function operate2(dig, sym){
             result *= digit;
         }else if(symbol === "/"){
             result /= digit;
+        }else if(symbol === "+"){
+            result += digit;
+        }else if(symbol === "-"){
+            result -= digit;
         }
     }
     return result;
+}
+
+function isMathSymbol(char) {
+    return char === "+" || char === "-" || char === "*" || char === "/";
 }
