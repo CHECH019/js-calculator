c872fef5ceb29263ed0082fba2d13966d6165636
Sergio Suarique <ss_1913@outlook.com>
2023-06-17 23:21:55 -0500

    calculator v1.1; minor bugs fixed, code optimizations & error handling

--- script.js
+++ script.js
@@ -1,39 +1,50 @@
-const buttons = [...document.getElementsByClassName("button")];
 const input = document.getElementById("input");
 const output = document.getElementById("output");
+const btns_container = document.getElementById('buttons-container');
 
-buttons.forEach(b =>{
-    b.addEventListener("mousedown", () => {
-        b.classList.toggle("onButtonPressed");
-        b.classList.remove("hover-style");
-    });
-    
-    b.addEventListener("mouseup", () => {
-        b.classList.remove("onButtonPressed");
-        b.classList.toggle("hover-style");
-    });
-  
-    b.addEventListener("click", () => {
-        if(b.classList.contains("digit")){
-            input.textContent += b.textContent;
-        }else if(b.textContent === "clr"){
-            input.textContent = "";
-            output.textContent = "";
-        }else if(b.textContent === "←"){
-            input.textContent = input.textContent.substring(0,input.textContent.length-1);
-        }else if(b.textContent === "="){
-            output.textContent = calculate(input.textContent);            
-        }
-    });
-    
-})
+btns_container.addEventListener('mousedown',e => {
+    e.target.classList.replace('hover','onButtonPressed');
+});
+btns_container.addEventListener("mouseup", (e) => {
+    e.target.classList.replace('onButtonPressed','hover');
+});
+btns_container.addEventListener("click", (e) => {
+    let b = e.target;
+    if(b.classList.contains("digit")){
+        input.textContent += b.textContent;
+    }else if(b.textContent === "clr"){
+        input.textContent = "";
+        output.textContent = "";
+    }else if(b.textContent === "←"){
+        input.textContent = input.textContent.substring(0,input.textContent.length-1);
+    }else if(b.textContent === "="){
+        let parsedInput = parseInput(input.textContent);
+        const operands = parsedInput.operands;
+        const operators = parsedInput.operators;
+        let error = validate(operands,operators);
+        output.textContent = error === '' ? calculate(operands.map(parseFloat),operators) : error;
+    }
+});
 
-function calculate(input) {
-    // Step 1: Parse the input string
-    const operands = input.match(/\d+\.?\d*/g).map(parseFloat);
-    const operators = input.match(/[\+\-\*\/]/g);
-    
-    // Step 2: Evaluate multiplication and division
+const parseInput = input => {
+    if(input.charAt(0) === '-') input = '0'+input;
+    let operands = input.match(/\d+\.?\d*/g);
+    let operators = input.match(/[\+\-\*\/]/g);
+    if (operators === null && operands.length === 1){
+        operands.unshift('0');
+        operators = ['+'];
+    }
+    return {operands,operators}
+}
+
+const validate = (operands, operators)=>{
+    if(operands === null || operators !== null && operands.length-1 !== operators.length) return 'Syntax Error';
+    if(operators.some((t,i) => t==='/' && operands[i+1] == 0)) return 'Math Error'
+    return '';
+}
+
+const calculate = (operands, operators) => {
+    // Step 1: Evaluate multiplication and division
     for (let i = 0; i < operators.length; i++) {
         if (operators[i] === '*' || operators[i] === '/') {
             operands[i] = performOperation(operators[i],operands[i],operands[i+1]);
@@ -43,26 +54,24 @@ function calculate(input) {
         }
     }
 
-    // Step 3: Evaluate addition and subtraction
+    // Step 2: Evaluate addition and subtraction
     let result = operands[0];
     for (let i = 1; i < operands.length; i++) {
         result = performOperation(operators[i-1],result,operands[i]);
     }
 
-    return result;
+    return (result+"").length > 10 ? parseFloat(result).toPrecision(10):result;
 }
-
-function performOperation(operator, a , b){
+const performOperation = (operator, a , b) => {
     switch (operator) {
         case '*':
             return a*b;
         case '/':
+            if(b == 0) throw new Error("Math Error: divison by zero");
             return a/b;
         case '+':
             return a+b;
         case '-':
             return a-b;
-        default:
-            throw new Error('Invalid operator: ' + operator);
     }
 }
