bd46229ecd2aeb5e205ea87b00ce47ac7f75be1b
Sergio Suarique <ss_1913@outlook.com>
2023-06-19 22:44:00 -0500

    readme and comments added

---
+++ README.md
@@ -0,0 +1,32 @@
+JavaScript Calculator
+This is a simple calculator built using JavaScript, HTML, and CSS. It allows users to perform basic arithmetic calculations such as addition, subtraction, multiplication, and division.
+
+Features
+Responsive and user-friendly calculator interface.
+Supports input of digits (0-9), decimal point (.), and basic arithmetic operators (+, -, *, /).
+Handles basic error cases such as division by zero and syntax errors.
+Provides immediate calculation results upon pressing the "=" button.
+Usage
+Clone the repository or download the project files.
+Open the index.html file in a web browser.
+Use the calculator by clicking the buttons for digits, decimal point, and arithmetic operators.
+The input and output fields are displayed at the top of the calculator.
+Press the "=" button to calculate the result. If there are any errors, they will be displayed in the output field.
+To clear the input and output fields, click the "clr" button.
+To delete the last character in the input field, click the "←" button.
+Technologies Used
+JavaScript
+HTML
+CSS
+File Structure
+index.html: The main HTML file that contains the calculator UI.
+script.js: The JavaScript file that handles the calculator logic.
+styles.css: The CSS file that applies styles to the calculator UI.
+Contributions
+Contributions to this calculator project are welcome. If you would like to suggest an improvement or report a bug, please open an issue or submit a pull request.
+
+License
+This JavaScript calculator is open source and available under the MIT License.
+
+Credits
+This calculator was developed by Sergio Suarique.
--- script.js
+++ script.js
@@ -1,77 +1,93 @@
-const input = document.getElementById("input");
-const output = document.getElementById("output");
-const btns_container = document.getElementById('buttons-container');
+// Get DOM elements
+const input = document.getElementById("input"); // Input field
+const output = document.getElementById("output"); // Output field
+const btns_container = document.getElementById('buttons-container'); // Buttons container
 
-btns_container.addEventListener('mousedown',e => {
-    e.target.classList.replace('hover','onButtonPressed');
+// Add event listeners to buttons container
+btns_container.addEventListener('mousedown', e => {
+    e.target.classList.replace('hover', 'onButtonPressed'); // Apply "onButtonPressed" style when button is pressed
 });
+
 btns_container.addEventListener("mouseup", (e) => {
-    e.target.classList.replace('onButtonPressed','hover');
+    e.target.classList.replace('onButtonPressed', 'hover'); // Revert back to "hover" style when button is released
 });
+
 btns_container.addEventListener("click", (e) => {
-    let b = e.target;
-    if(b.classList.contains("digit")){
-        input.textContent += b.textContent;
-    }else if(b.textContent === "clr"){
-        input.textContent = "";
-        output.textContent = "";
-    }else if(b.textContent === "←"){
-        input.textContent = input.textContent.substring(0,input.textContent.length-1);
-    }else if(b.textContent === "="){
+    let b = e.target; // Clicked button
+
+    if (b.classList.contains("digit")) {
+        input.textContent += b.textContent; // Append clicked digit to input field
+    } else if (b.textContent === "clr") {
+        input.textContent = ""; // Clear input field
+        output.textContent = ""; // Clear output field
+    } else if (b.textContent === "←") {
+        input.textContent = input.textContent.substring(0, input.textContent.length - 1); // Remove last character from input field
+    } else if (b.textContent === "=") {
+        // Parse input, validate, and calculate result
         let parsedInput = parseInput(input.textContent);
         const operands = parsedInput.operands;
         const operators = parsedInput.operators;
-        let error = validate(operands,operators);
-        output.textContent = error === '' ? calculate(operands.map(parseFloat),operators) : error;
+        let error = validate(operands, operators);
+        output.textContent = error === '' ? calculate(operands.map(parseFloat), operators) : error; // shows the operation result if there's no errors, otherwise shows the error
     }
 });
 
+// Parse input string into operands and operators
 const parseInput = input => {
-    if(input.charAt(0) === '-') input = '0'+input;
-    let operands = input.match(/\d+\.?\d*/g);
-    let operators = input.match(/[\+\-\*\/]/g);
-    if (operators === null && operands.length === 1){
-        operands.unshift('0');
+    if (input.charAt(0) === '-') input = '0' + input; // Prepend '0' if input starts with a negative sign
+    let operands = input.match(/\d+\.?\d*/g); // Extract numeric operands
+    let operators = input.match(/[\+\-\*\/]/g); // Extract arithmetic operators
+
+    if (operators === null && operands.length === 1) {
+        operands.unshift('0'); // Prepend '0' if there are no operators and only one operand
         operators = ['+'];
     }
-    return {operands,operators}
-}
 
-const validate = (operands, operators)=>{
-    if(operands === null || operators !== null && operands.length-1 !== operators.length) return 'Syntax Error';
-    if(operators.some((t,i) => t==='/' && operands[i+1] == 0)) return 'Math Error'
-    return '';
-}
+    return { operands, operators };
+};
 
+// Validate operands and operators for syntax and math errors
+const validate = (operands, operators) => {
+    if (operands === null || (operators !== null && operands.length - 1 !== operators.length)) {
+        return 'Syntax Error'; // Invalid syntax if operands and operators don't match
+    }
+    if (operators.some((t, i) => t === '/' && operands[i + 1] == 0)) {
+        return 'Math Error'; // Division by zero error
+    }
+    return ''; // return '' if there's no errors;
+};
+
+// Perform arithmetic calculations
 const calculate = (operands, operators) => {
     // Step 1: Evaluate multiplication and division
     for (let i = 0; i < operators.length; i++) {
         if (operators[i] === '*' || operators[i] === '/') {
-            operands[i] = performOperation(operators[i],operands[i],operands[i+1]);
-            operands.splice(i + 1, 1);
-            operators.splice(i, 1);
-            i--;
+            operands[i] = performOperation(operators[i], operands[i], operands[i + 1]); // Perform operation
+            operands.splice(i + 1, 1); // Remove next operand from array
+            operators.splice(i, 1); // Remove operator from array
+            i--; // Decrement index to account for removed elements
         }
     }
 
     // Step 2: Evaluate addition and subtraction
     let result = operands[0];
     for (let i = 1; i < operands.length; i++) {
-        result = performOperation(operators[i-1],result,operands[i]);
+        result = performOperation(operators[i - 1], result, operands[i]); // Perform operation
     }
 
-    return (result+"").length > 10 ? parseFloat(result).toPrecision(10):result;
-}
-const performOperation = (operator, a , b) => {
+    return (result + "").length > 10 ? parseFloat(result).toPrecision(10) : result; // Limit result to 10 decimal places
+};
+
+// Perform individual arithmetic operation
+const performOperation = (operator, a, b) => {
     switch (operator) {
         case '*':
-            return a*b;
+            return a * b;
         case '/':
-            if(b == 0) throw new Error("Math Error: divison by zero");
-            return a/b;
+            return a / b;
         case '+':
-            return a+b;
+            return a + b;
         case '-':
-            return a-b;
+            return a - b;
     }
-}
+};
